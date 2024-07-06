6897c9099617fcf0c69ea6cc1b6dd11058cd75b5
remy <rahimjangi1982@gmail.com>
2024-07-06 22:16:59 +0200

    MemberService added to CLIENT

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { RegisterComponent } from '../register/register.component';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../_models/user.model';
+import { MembersService } from '../_services/members.service';
 
 @Component({
   selector: 'app-home',
@@ -10,30 +11,14 @@ import { User } from '../_models/user.model';
   styleUrl: './home.component.css',
   imports: [RegisterComponent],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   registerMode = false;
-  http = inject(HttpClient);
   users: any;
 
   registerToggle() {
     this.registerMode = !this.registerMode;
   }
-  ngOnInit(): void {
-    this.getUsers();
-  }
-  getUsers() {
-    this.http.get<User[]>('https://localhost:5001/api/users').subscribe({
-      next: (response) => {
-        this.users = response;
-      },
-      error: (err) => {
-        console.log(err);
-      },
-      complete: () => {
-        console.log('request has completed');
-      },
-    });
-  }
+
   cancelRegisterMode(event: boolean) {
     this.registerMode = event;
   }
